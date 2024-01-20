2b19ffc4f7c9983e3b625c1d6cf6d913eb43822c
Hlabeli <hlabeli.zee@gmail.com>
2024-01-20 22:44:11 +0200

    Initial commit

---
+++ src/main/java/com/mespay/MespayUssdApplication.java
@@ -0,0 +1,13 @@
+package com.mespay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MespayUssdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MespayUssdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mespay/MespayUssdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mespay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MespayUssdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
